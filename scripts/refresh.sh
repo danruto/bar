a18e7943944b6a5ef73b1f5dea2045a5b1361b76
Adwin Ying <adwinying@gmail.com>
2018-12-31 20:09:48 +0900

    Less obtrusive refresh

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-osascript -e 'tell application "Übersicht" to refresh'
+# osascript -e 'tell application "Übersicht" to refresh'
+touch $HOME/Library/Application\ Support/Übersicht/widgets/bar/index.jsx
