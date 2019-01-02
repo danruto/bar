135bdd24301d18c08d967b3ec6767f45694a0abc
Adwin Ying <adwinying@gmail.com>
2019-01-03 03:57:21 +0900

    Update refresh script

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# osascript -e 'tell application "Übersicht" to refresh'
-touch $HOME/Library/Application\ Support/Übersicht/widgets/bar/index.jsx
+touch $HOME/Library/Application\ Support/Übersicht/widgets/bar/components/$1.jsx
