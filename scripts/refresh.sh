a0d7ce2f85c329ee6c19661caca007c6e1c371e0
Adwin Ying <adwinying@gmail.com>
2019-01-03 16:35:39 +0900

    Prevent refresh script from running without specifying component to refresh

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [ "$1" = "" ]; then
+  echo "[Error] Please specify component to refresh."
+  exit 1
+fi
+
 touch $HOME/Library/Application\ Support/Übersicht/widgets/bar/components/$1.jsx
